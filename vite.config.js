1f6e938d6ebdb136335cbe2039250fbddfdf26e3
Lucas Santana <163876124+LucasAlexSant@users.noreply.github.com>
2024-11-08 23:13:34 -0300

    ajustado para liveshare

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: true, // ou coloque '0.0.0.0'
+    port: 5173, // você pode alterar para qualquer porta disponível
+  }
+});
+
+
+
+
+  
