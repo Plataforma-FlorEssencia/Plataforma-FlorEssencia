783ca425c371258e4f8debd53bd41b140dfe7593
Augusto <62222041+coffeasack@users.noreply.github.com>
2024-11-10 15:11:25 -0300

    update na pagina consulta

--- src/Pages/ShowCase/ShowCase.jsx
+++ src/Pages/ShowCase/ShowCase.jsx
@@ -1,32 +1,84 @@
+import React from "react";
 import Header from "../../components/Header/Header";
 import FooterPreLogin from "../../components/Footer/Footer";
+import anap from "../ShowCase/assets/psicologa-ana.png";
+
 import "./ShowCase.css";
 
-function ShowCase() {
+const ShowCase = () => {
     return (
-    <div>
+        <div>
+            <div>
+                <Header />
+            </div>
+            <div className="consulta">
+                <div className="card-container">
+                    <div className="card-header">
+                        <span className="crp">CRP: 05/73060</span>
+                    </div>
+                    
+                    <div className="card-body">
+                    <img src={anap} alt="Ana Paula Evaldt Correa" className="profile-pic"/>
+                        <div className="card-info">
+                    
+                            <div className="specializations">
+                                <h4>Especialista em</h4>
+                                <button className="specialization-btn">Traumas</button>
+                                <button className="specialization-btn">Relacionamentos</button>
+                                <button className="specialization-btn">Frustrações</button>
+                            </div>
+                            <div className="approach">
+                                <h4>Abordagem</h4>
+                                <button className="approach-btn">Psicanálise - Lacan</button>
+                            </div>
+                            
+                        </div>
+                    </div>
+                    
+                    <h3 className="name">Ana Paula Evaldt Correa</h3>
+                    <div className="contact-btn">
+                        <button className="whatsapp-btn">Contate-me!</button>
+                    </div>
+                </div>
+            </div>
+            <div>
+                <FooterPreLogin />
+            </div>
+        </div>
 
-    <div>   
-        <Header />
-    </div>  
-    <div className="consulta">
-    <div className="vitrine">
-        <div className="vitrine-separado-esquerda"></div>
-        <div className="vitrine-separado-direita"></div>
-
-    </div>
-
-    <div className="vitrine">
-        <div className="vitrine-separado-esquerda"></div>
-        <div className="vitrine-separado-direita"></div>
-    </div>
-    </div>
-    <div>   
-        <FooterPreLogin />
-    </div> 
-
-    </div>
-    )
+    );
 };
+
+/* function ShowCase() {
+    return (
+      
+      
+      
+      
+         <div>
+
+            <div>
+                <Header />
+            </div>
+            <div className="consulta">
+                <div className="vitrine">
+                    <div className="vitrine-separado-esquerda"></div>
+                    <div className="vitrine-separado-direita"></div>
+
+                </div>
+
+                <div className="vitrine">
+                    <div className="vitrine-separado-esquerda"></div>
+                    <div className="vitrine-separado-direita"></div>
+                </div>
+            </div>
+
+            <div>
+                <FooterPreLogin />
+            </div>
+
+        </div> 
+    )
+}; */
 
 export default ShowCase;
