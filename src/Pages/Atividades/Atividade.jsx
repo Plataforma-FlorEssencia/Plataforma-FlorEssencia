b0897d846f8bb0acac6536c3d3a854258aa2a859
Lucas Santana <163876124+LucasAlexSant@users.noreply.github.com>
2024-11-14 23:20:20 -0300

    pagina atividades, inicio

--- src/Pages/Atividades/Atividade.jsx
+++ src/Pages/Atividades/Atividade.jsx
@@ -1,17 +1,40 @@
 import React from 'react';
 import SideBar from "../../components/SideBar/SideBar";
-import './Atividade.css'
+import { FaBook, FaPuzzlePiece, FaChartLine, FaCogs, FaRocket } from 'react-icons/fa';
+import './Atividade.css';
+
 
 function Atividade() {
     return (
         <div className="main-atividades">
             <SideBar />
-            <div className="body-atividade">
-                <section className='hero-atividade'>
-                    <h1 class="display-1 fw-bolder text-capitalize">OLÁ ADRIANA</h1>
-                    
 
-                </section>
+            <div className="body-atividade">
+                <header className="hero-atividade">
+                    <h1 className="display-1 fw-bolder text-capitalize">OLÁ, ADRIANA</h1>
+                </header>
+
+                <div className="grid-container">
+                    <div className="grid-item" style={{ gridRow: 1, gridColumn: 51 }}>
+                    <div className="atividade-btn"><img src="/vasinho.svg" alt="vaso" /></div>
+                    </div>
+                    <div className="grid-item" style={{ gridRow: 2, gridColumn: 52 }}>
+                    <div className="atividade-btn"><img src="/vasinho.svg" alt="vaso" /></div>
+                    </div>
+                    <div className="grid-item" style={{ gridRow: 3, gridColumn: 53 }}>
+                    <div className="atividade-btn"><img src="/vasinho.svg" alt="vaso" /></div>
+                    </div>
+                    <div className="grid-item" style={{ gridRow: 4, gridColumn: 52 }}>
+                    <div className="atividade-btn"><img src="/vasinho.svg" alt="vaso" /></div>
+                    </div>
+                    <div className="grid-item" style={{ gridRow: 5, gridColumn: 51 }}>
+                    <div className="atividade-btn"><img src="/vasinho.svg" alt="vaso" /></div>
+                    </div>
+                    <div className="grid-item" style={{ gridRow: 10, gridColumn: 52 }}>
+                        <div className="atividade-btn"><img src="/vasinho.svg" alt="vaso" /></div>
+                    </div>
+
+                </div>
             </div>
         </div>
     );
