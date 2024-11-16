428da4aa0d17798522ac4b3e04727f8ff270f43f
Augusto <62222041+coffeasack@users.noreply.github.com>
2024-11-16 11:50:12 -0300

    tentativa de pag pagamento com surveyjs

---
+++ src/Pages/Pagamento/Pagamento.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import SurveyComponent from "../../components/SurveyComponent/SurveyComponent";
+import Sidebar from "../../components/SideBar/SideBar";
+
+const Pagamento = () => {
+    return (
+        <div>
+        <Sidebar/>
+        <SurveyComponent/>
+        </div>
+    )
+};
+
+export default Pagamento;
