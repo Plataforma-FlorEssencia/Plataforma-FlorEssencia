891df77ff11d489b1c6a3fdda64ea512a5843872
Lucas Santana <163876124+LucasAlexSant@users.noreply.github.com>
2024-11-27 18:54:16 -0300

    Revert "deploy"
    This reverts commit 7febdf28f666d311ed9fff47fdb8fa1bf1656b07.

--- vite.config.js
+++ vite.config.js
@@ -7,8 +7,7 @@ export default defineConfig({
   server: {
     host: true, // ou coloque '0.0.0.0'
     port: 5173, // você pode alterar para qualquer porta disponível
-  },
-  base:"/Plataforma-FlorEssencia"
+  }
 });
 
 
